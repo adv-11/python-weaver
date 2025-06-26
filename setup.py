5e0529ce3fba41b13ec4171f67fa7b2f1662826e
Advait Shinde <105921012+adv-11@users.noreply.github.com>
2025-06-26 10:46:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-weaver",
-    version="0.1.0",
+    version="0.1.1",
     author="Advait Shinde",
     author_email="advaitss11@gmail.com",
     description="A framework to orchestrate long-running LLM workflows with a persistent task tracker.",
